60278803f82f6a81aa15fc74701a550ac0632ce0
Ken Thumi <kenthumi@gmail.com>
2021-05-15 22:36:44 +0300

    Models Category & Location added

--- photos/models.py
+++ photos/models.py
@@ -9,6 +9,8 @@ class Image(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField()
     image = CloudinaryField('image')
+    category = models.ForeignKey('Category',on_delete=models.CASCADE,null=True)
+    location = models.ForeignKey('Location',on_delete=models.CASCADE,null=True)
 
 
     def save_image(self):
@@ -16,3 +18,17 @@ class Image(models.Model):
 
     def __str__(self):
         return f'Image: {self.name}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
