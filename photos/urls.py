0fe6071824f416feaf233dc4799c857551b0cdae
Ken Thumi <kenthumi@gmail.com>
2021-05-17 01:02:38 +0300

    Readme,licance,ERD added

--- photos/urls.py
+++ photos/urls.py
@@ -3,7 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-    # url('^$',views.welcome,name = 'welcome'),
     path('',views.home,name = 'home'),
     path('upload',views.upload, name='upload'),
     path('edit/<int:image_id>',views.edit, name='edit'),
